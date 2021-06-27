ad937fefe0c02546431c20a6616f1b56f8292bb8
Lars Beukenkamp <lars@beukenkamp.net>
2021-06-27 16:58:39 +0200

    lol it really was too hard so even easier

--- dev/game.ts
+++ dev/game.ts
@@ -36,7 +36,7 @@ class Game {
     
             //checking to spawn new ghosts
             if (this.ghostSpawnTimer > 1) {
-                let amount = (Math.random() * 9 + (this.time / 10))
+                let amount = (Math.random() * 9 + (this.time / 20))
                 this.chamber.createGhosts(amount)
                 this.ghostSpawnTimer = 0
 
--- docs/js/game.js
+++ docs/js/game.js
@@ -19,7 +19,7 @@ class Game {
             this.ghostSpawnTimer += 0.005;
             this.chamber.update(this.time);
             if (this.ghostSpawnTimer > 1) {
-                let amount = (Math.random() * 9 + (this.time / 10));
+                let amount = (Math.random() * 9 + (this.time / 20));
                 this.chamber.createGhosts(amount);
                 this.ghostSpawnTimer = 0;
                 if (this.chamber.getPoints() >= 10) {
